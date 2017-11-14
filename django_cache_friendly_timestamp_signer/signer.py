96adff6160f712ff826c09e43ae61d773c0ec262
Raphael Riel <rriel@poka.io>
2017-11-14 11:35:25 -0500

    Code documentation

--- django_cache_friendly_timestamp_signer/signer.py
+++ django_cache_friendly_timestamp_signer/signer.py
@@ -9,6 +9,19 @@ from django.utils import baseconv
 class TimeFramedTimestampSigner(signing.TimestampSigner):
 
     def __init__(self, time_frame, uniform_distribution=True, **kwargs):
+        """
+        :param time_frame: Duration in either `datetime.timedelta` object
+               or int representing seconds.
+               Within this duration, the signature will not change.
+               Make sure this duration is less than the TTL/max_age
+               of the signature.
+        :param uniform_distribution: Boolean indicating if the signature's
+               timestamp should be pseudo-randomly
+               placed within the given time_frame.
+               Enabling this will ensure that multiple signatures'
+               rotation won't happen at the very same moment.
+               Defaults to True
+        """
         if isinstance(time_frame, timedelta):
             self.time_frame_seconds = time_frame.total_seconds()
 
@@ -35,9 +48,12 @@ class TimeFramedTimestampSigner(signing.TimestampSigner):
     def timestamp(self):
         original = int(time.time())
 
+        # Start of time frame.
         timestamp = original - (original % self.time_frame_seconds)
 
         if self.uniform_distribution:
+            # Make sure that for a given value,
+            # the "random" delay is always the same.
             random.seed(self._uniform_distribution_salt)
             delay = random.uniform(0, self.time_frame_seconds)
             timestamp += delay
